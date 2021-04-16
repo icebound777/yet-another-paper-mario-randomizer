b117b9d5da6e360238ba888d07220b0322363c63
Icebound777 <frozenblue@gmx.de>
2021-04-17 00:51:07 +0200

    mapped out some locations by byte

--- YaPMRandomizer.py
+++ YaPMRandomizer.py
@@ -1,13 +1,14 @@
 import os
 
 from statics.version import __version__
-from bytelocations.bytes import bl_starting_location
+from bytelocations.bytes import bl_starting_location, byte_locations
 
 print(f'YaPMR v.{__version__}')
 
 infile = 'Paper Mario (U) [!].z64'
 outfile = 'PMRandomized.z64'
 
+# Open Rom and check if it is actually a PM rom
 assert(os.path.isfile(infile))
 os.system('copy "%s" "%s"' % (infile, outfile))
 f=open(outfile,'rb+')
@@ -15,10 +16,15 @@ f=open(outfile,'rb+')
 f.seek(0x20)
 assert(f.read(11) == b'PAPER MARIO')
 
-#don't start the game from mario's house
+# Don't start the game from Mario's house
+#print(f'{f.tell()}')
 f.seek(bl_starting_location)
-f.write((0x24020000).to_bytes(4,'big'))
+#print(f'{f.tell()}')
+#f.write(byte_locations.get('kmr_03'))
+f.write((0x2402004F).to_bytes(4,'big'))
+#print(f'{f.tell()}')
 
+# Save randomized file and fix bootcode
 f.close()
 assert(os.path.isfile('rn64crc2\\rn64crc.exe'))
 os.system('rn64crc2\\rn64crc.exe "%s" -u' % outfile)
